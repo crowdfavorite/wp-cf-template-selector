b40a951cc7abfd272aeeb6371831d6e140b09c0c
sklein <sklein@546ca710-db30-0410-8887-d9dd8e66aff0>
2010-05-13 23:22:07 +0000

    initial commit of the template selector
    git-svn-id: https://git.crowdfave.org/svn/crowdfavorite/code/wp-plugins/cf-template-selector/trunk@24358 546ca710-db30-0410-8887-d9dd8e66aff0

---
+++ cf-template-selector.php
@@ -0,0 +1,223 @@
+<?php 
+/*
+Plugin Name: CF Template Selector
+Plugin URI: http://crowdfavorite.com
+Description: Custom Template Selector for the Page edit screen.  Adds the ability to see screenshots and descriptions of page template files.
+Version: 1.0
+Author: Crowd Favorite
+Author URI: http://crowdfavorite.com
+*/
+
+// ini_set('display_errors', '1'); ini_set('error_reporting', E_ALL);
+
+## Constants
+define('CFTS_VERSION', '1.0');
+define('CFTS_DIR', trailingslashit(realpath(dirname(__FILE__))));
+
+if (!defined('PLUGINDIR')) {
+	define('PLUGINDIR','wp-content/plugins');
+}
+
+## Includes
+
+load_plugin_textdomain('cfts');
+
+## Init Functionality
+
+function cfts_resources() {
+	if (!empty($_GET['cf_action'])) {
+		switch ($_GET['cf_action']) {
+			case 'cfts_admin_js':
+				cfts_admin_js();
+				die();
+				break;
+			case 'cfts_admin_css':
+				cfts_admin_css();
+				die();
+				break;
+		}
+	}
+}
+add_action('init', 'cfts_resources', 1);
+
+
+## JS/CSS
+
+function cfts_admin_js() {
+	header('Content-type: text/javascript');
+	?>
+	;(function($) {
+		$(function() {
+			$(".cfts-option-radio").live('change', function() {
+				var _this = $(this);
+				var id = _this.attr('id');
+				var file = $("#cfts-option-file-"+id.replace('cfts-option-radio-', '')).val();
+				$("#page_template").val(file);
+			});
+		});
+	})(jQuery);
+	<?php
+	die();
+}
+
+if (is_admin()) {
+	wp_enqueue_script('cfts_admin_js', admin_url('?cf_action=cfts_admin_js'), array('jquery'), CFTS_VERSION);
+}
+
+function cfts_admin_css() {
+	header('Content-type: text/css');
+	?>
+	.cfts-identifier {
+		font-weight:bold;
+	}
+	<?php
+	die();
+}
+
+if (is_admin()) {
+	wp_enqueue_style('cfts_admin_css', admin_url('?cf_action=cfts_admin_css'), array(), CFTS_VERSION, 'screen');
+}
+
+
+## Page Display Functionality
+
+function cfts_page_template_selector() {
+	global $post;
+	$template_info = cfts_page_template_info();
+	if (!is_array($template_info) || empty($template_info)) { return; }
+	
+	$selected = $template_info[$post->page_template];
+	?>
+	<div id="cfts-page-template-selector-area" style="display:none;">
+		<div id="cfts-page-template-selector" class="cfts-page-template-selector">
+			<span id="cfts-selected" class="cfts-value">
+				<input type="radio" name="cfts-option-radio" id="cfts-option-radio-<?php echo $post->page_template; ?>" class="cfts-option-radio" checked="checked" />
+				<label for="cfts-option-radio-<?php echo $post->page_template; ?>">
+					<?php
+					if (!empty($selected['screenshot'])) {
+						echo '<img src="'.trailingslashit(get_stylesheet_directory_uri()).$selected['screenshot'].'" id="cfts-selected-screenshot" class="cfts-screenshot">';
+					}
+					if (!empty($selected['name'])) {
+						echo '<span id="cfts-selected-name" class="cfts-name"><span class="cfts-identifier">Name: </span>'.$selected['name'].'</span>';
+					}
+					if (!empty($selected['description'])) {
+						echo '<span id="cfts-selected-description" class="cfts-description"><span class="cfts-identifier">Description: </span>'.$selected['description'].'</span>';
+					}
+					?>
+				</label>
+			</span>
+			<ul class="cfts-options">
+				<?php 
+				foreach ($template_info as $filename => $template) { 
+					if ($filename == $post->page_template) { continue; }
+					$file = str_replace('.php', '', $filename);
+					?>
+					<li id="cfts-option-<?php echo $file; ?>" class="cfts-option">
+						<input type="radio" name="cfts-option-radio" id="cfts-option-radio-<?php echo $file; ?>" class="cfts-option-radio"<?php checked($filename, $post->page_template); ?> />
+						<input type="hidden" id="cfts-option-file-<?php echo $file; ?>" value="<?php echo $filename; ?>" />
+						<label for="cfts-option-radio-<?php echo $file; ?>">
+							<?php
+							if (!empty($template['screenshot'])) {
+								echo '<img src="'.trailingslashit(get_stylesheet_directory_uri()).$template['screenshot'].'" id="cfts-option-screenshot-'.$file.'" class="cfts-screenshot">';
+							}
+							if (!empty($template['name'])) {
+								echo '<span id="cfts-option-name-'.$file.'" class="cfts-name"><span class="cfts-identifier">Name: </span>'.$template['name'].'</span>';
+							}
+							if (!empty($template['description'])) {
+								echo '<span id="cfts-option-description-'.$file.'" class="cfts-description"><span class="cfts-identifier">Description: </span>'.$template['description'].'</span>';
+							}
+							?>
+						</label>
+					</li>
+				<?php 
+				} 
+				?>
+			</ul>
+		</div>
+	</div>
+	<script type="text/javascript">
+		jQuery("#page_template").hide();
+		jQuery("#page_template").after(jQuery("#cfts-page-template-selector-area").html());
+	</script>
+	<?php
+}
+add_action('admin_footer', 'cfts_page_template_selector');
+
+
+## Page Template Info Gathering Functionality
+
+function cfts_page_template_info() {
+	$themes = get_themes();
+	$theme = get_current_theme();
+	$templates = $themes[$theme]['Template Files'];
+	$page_templates = array();
+
+	if (is_array($templates) && !empty($templates)) {
+		$base = array(trailingslashit(get_template_directory()), trailingslashit(get_stylesheet_directory()));
+		
+		if (file_exists(trailingslashit(get_stylesheet_directory()).'page.php')) {
+			$basename = 'page.php';
+			$template_data = implode('', file(trailingslashit(get_stylesheet_directory()).'page.php'));
+			$name = '';
+			$description = '';
+			$screenshot = '';
+			
+			if (preg_match('|Template Name:(.*)$|mi', $template_data, $name)) {
+				$name = _cleanup_header_comment($name[1]);
+			}
+			else {
+				$name = 'Default';
+				$basename = 'default';
+			}
+			if (preg_match('|Description:(.*)$|mi', $template_data, $description)) {
+				$description = _cleanup_header_comment($description[1]);
+			}
+			if (preg_match('|Screenshot:(.*)$|mi', $template_data, $screenshot)) {
+				$screenshot = _cleanup_header_comment($screenshot[1]);
+			}
+			
+			if (!empty($name)) {
+				$page_templates[$basename] = array(
+					'name' => $name,
+					'description' => $description,
+					'screenshot' => $screenshot
+				);
+			}
+		}
+		
+		foreach ($templates as $template) {
+			$basename = str_replace($base, '', $template);
+			
+			// Don't allow template files in subdirectories???
+			if (strpos($basename, '/') !== false) {
+				continue;
+			}
+			
+			$template_data = implode('', file($template));
+			$name = '';
+			$description = '';
+			$screenshot = '';
+			if (preg_match('|Template Name:(.*)$|mi', $template_data, $name)) {
+				$name = _cleanup_header_comment($name[1]);
+			}
+			if (preg_match('|Description:(.*)$|mi', $template_data, $description)) {
+				$description = _cleanup_header_comment($description[1]);
+			}
+			if (preg_match('|Screenshot:(.*)$|mi', $template_data, $screenshot)) {
+				$screenshot = _cleanup_header_comment($screenshot[1]);
+			}
+			
+			if (!empty($name)) {
+				$page_templates[$basename] = array(
+					'name' => $name,
+					'description' => $description,
+					'screenshot' => $screenshot
+				);
+			}
+		}
+	}
+	ksort($page_templates);
+	return $page_templates;
+}
+
+?>
